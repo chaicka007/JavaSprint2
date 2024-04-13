94bff9be70ede3cb0ceb0e89a9956590d0aa7e46
chaicka007 <sashasa534@gmail.com>
2024-04-13 21:18:22 +0400

    initial release

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        TaskManager taskManager = new TaskManager();
+        taskManager.newTask(new Task("Задача 1", "Описание 2 задачи"));
+        taskManager.newTask(new Task("Задача 2", "Описание 2 супер задачи"));
+        taskManager.newEpic(new Epic("Эпик 1", ""));
+        taskManager.newEpic(new Epic("эпик 2", ""));
+        taskManager.newSubtask(new Subtask("Подзадача 1 эпика 1", "ее описание", 2));
+        taskManager.newSubtask(new Subtask("Подзадача 2 эпика 1", "ее описание", 2));
+        taskManager.newSubtask(new Subtask("Подзадача 1 эпика 2", "ее описание", 3));
+        System.out.println(taskManager.getTasks());
+        System.out.println(taskManager.getEpics());
+        System.out.println(taskManager.getSubtasks());
+        System.out.println(taskManager.getSubtaskByEpicId(2));
+        taskManager.updateSubtask(new Subtask("Подзадача 2 эпика 1", "ее описание",
+                Task.Status.DONE, 5, 2));
+        System.out.println("Проверка после изменения одной подзадачи 1 эпика");
+        System.out.println(taskManager.getSubtaskByEpicId(2));
+        System.out.println(taskManager.getEpics());
+        System.out.println("Проверка после изменения 1 подзадачи 2 эпика");
+        taskManager.updateSubtask(new Subtask("Подзадача 1 эпика 2", "ее описание",
+                Task.Status.DONE, 6, 3));
+        System.out.println(taskManager.getSubtaskByEpicId(3));
+        System.out.println(taskManager.getEpics());
+        System.out.println("Проверка после удаления 1 эпика:");
+        taskManager.deleteEpicById(2);
+        System.out.println(taskManager.getSubtasks());
+        System.out.println(taskManager.getEpics());
+        System.out.println("проверка после удаления подзадачи 2 эпика");
+        taskManager.deleteSubtaskById(6);
+        System.out.println(taskManager.getEpics());
+    }
+}
---
+++ src/Task.java
@@ -0,0 +1,74 @@
+import java.util.Objects;
+
+public class Task {
+    private String name;
+    private String description;
+    private Status status;
+    private long id;
+
+    public enum Status {
+        NEW,
+        IN_PROGRESS,
+        DONE;
+    }
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.status = Status.NEW;
+    }
+
+    public Task(String name, String description, Status status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Task(String name, String description, Status status, long id) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(name, task.name) && Objects.equals(description, task.description) && status == task.status;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(name, description, status);
+        result = 31 * result;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", id=" + id +
+                '}';
+    }
+}
