9f2a6acd94d30cc2d9116c4a04ef61cf7b6d4c19
chaicka007 <sashasa534@gmail.com>
2024-06-28 19:00:34 +0400

    feature: новый FileBackedTasksManager хранящий базу в файле Небольшие изменения в inMemoryTaskManager

--- src/managers/FileBackedTasksManager.java
+++ src/managers/FileBackedTasksManager.java
@@ -8,7 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
 
 public class FileBackedTasksManager extends InMemoryTaskManager {
@@ -17,131 +17,140 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
 
 
     public static void main(String[] args) {
-          FileBackedTasksManager taskManager = FileBackedTasksManager.loadFromFile();
-//        FileBackedTasksManager taskManager = new FileBackedTasksManager();
-//        taskManager.newTask(new Task("Задача 1", "Описание 1 задачи"));
-//        taskManager.newTask(new Task("Задача 2", "Описание 2 супер задачи"));
-//        taskManager.newTask(new Task("Задача 3", "Описание 3 супер задачи"));
-//        taskManager.newTask(new Task("Задача 4", "desc4", Status.NEW, 33));
-//        taskManager.newEpic(new Epic("Эпик 1", "asasa"));
-//        taskManager.newEpic(new Epic("эпик 2", "sasa"));
-//        taskManager.newEpic(new Epic("эпик 3", "sasa"));
-//        taskManager.newSubtask(new Subtask("Подзадача 1 эпика 1", "ее описание", 4));
-//        taskManager.newSubtask(new Subtask("Подзадача 2 эпика 1", "ее описание", 4));
-//        taskManager.newSubtask(new Subtask("Подзадача 1 эпика 2", "ее описание", 5));
-//        taskManager.getTask(1);
-//        taskManager.getTask(2);
-//        taskManager.getTask(3);
-//        taskManager.getTask(1);
-//        taskManager.getTask(1);
-//        System.out.println(taskManager.getTask(33));
-//        System.out.println(taskManager.getHistory()); //Должен быть список просмотра без повтора в порядке 2,3,1
-//        taskManager.deleteTaskById(2);
-//        System.out.println(taskManager.getHistory()); //Должен быть список просмотра без повтора в порядке 3,1
-//        taskManager.getTask(6);
-//        taskManager.getTask(6);
-//        System.out.println(taskManager.getHistory()); //3,1,6
-//        taskManager.getTask(4);
-//        taskManager.getTask(7);
-//        taskManager.getTask(8);
-//        System.out.println(taskManager.getHistory()); //3,1,6,4,7,8
-//        taskManager.save();
-//        System.out.println(taskManager.getTasks());
-//        System.out.println(taskManager.getEpics());
-//        System.out.println(taskManager.getSubtasks());
-          System.out.println(taskManager.getHistory());
+        while (true) {
+            System.out.println("Что делаем?\n 1. Тестируем сохранение в save.csv \n" +
+                    "2. Тестируем загрузку с файла\n" +
+                    "3. Тестируем выброс исключения");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    testCreateTasksAndHistory();
+                    break;
+                case 2:
+                    testLoadFromFileAndPrintHistory();
+                    break;
+                case 3:
+                    testException();
+                    break;
+                case 0:
+                    return;
+            }
+        }
+    }
 
+    public static void testCreateTasksAndHistory() {
+        FileBackedTasksManager taskManager = new FileBackedTasksManager();
+        taskManager.newTask(new Task("Задача 1", "Описание 1 задачи"));
+        taskManager.newTask(new Task("Задача 2", "Описание 2 супер задачи"));
+        taskManager.newTask(new Task("Задача 3", "Описание 3 супер задачи"));
+        taskManager.newTask(new Task("Задача 4", "desc4", Status.NEW, 33));
+        taskManager.newEpic(new Epic("Эпик 1", "asasa"));
+        taskManager.newEpic(new Epic("эпик 2", "sasa"));
+        taskManager.newEpic(new Epic("эпик 3", "sasa"));
+        taskManager.newSubtask(new Subtask("Подзадача 1 эпика 1", "ее описание", 4));
+        taskManager.newSubtask(new Subtask("Подзадача 2 эпика 1", "ее описание", 4));
+        taskManager.newSubtask(new Subtask("Подзадача 1 эпика 2", "ее описание", 5));
+        taskManager.getTask(1);
+        taskManager.getTask(2);
+        taskManager.getTask(3);
+        taskManager.getTask(4);
+        taskManager.getTask(7);
+        taskManager.getTask(8);
+        System.out.println(taskManager.getHistory());
+    }
+
+    public static void testLoadFromFileAndPrintHistory() {
+        FileBackedTasksManager taskManager = FileBackedTasksManager.loadFromFile("save.csv");
+        System.out.println("История " + taskManager.getHistory());
+        System.out.println("Таски " + taskManager.getTasks());
+        System.out.println("Эпики " + taskManager.getEpics());
+        System.out.println("Саб таски " + taskManager.getSubtasks());
+    }
+
+    public static void testException() {
+        FileBackedTasksManager taskManager = FileBackedTasksManager.loadFromFile("abraKadabra.csv");
     }
 
 
     @Override
     public void newTask(Task newTask) {
         super.newTask(newTask);
+        save();
     }
 
     @Override
     public void newEpic(Epic newEpic) {
         super.newEpic(newEpic);
+        save();
+    }
+
+    @Override
+    public Task getTask(long id) {
+        super.getTask(id);
+        save();
+        return super.getTask(id);
+
     }
 
     @Override
     public void newSubtask(Subtask newSubtask) {
         super.newSubtask(newSubtask);
+        save();
     }
 
     @Override
     public void updateTask(Task updateTask) {
         super.updateTask(updateTask);
+        save();
     }
 
     @Override
     public void updateEpic(Epic updateEpic) {
         super.updateEpic(updateEpic);
+        save();
     }
 
     @Override
     public void updateSubtask(Subtask updateSubtask) {
         super.updateSubtask(updateSubtask);
+        save();
     }
 
     @Override
     public void deleteTaskById(long id) {
         super.deleteTaskById(id);
+        save();
     }
 
     @Override
     public void deleteEpicById(long id) {
         super.deleteEpicById(id);
+        save();
     }
 
     @Override
     public void deleteSubtaskById(long id) {
         super.deleteSubtaskById(id);
-    }
-
-    @Override
-    public Task getTask(long id) {
-        return super.getTask(id);
-    }
-
-    @Override
-    public List<Subtask> getSubtaskByEpicId(long id) {
-        return super.getSubtaskByEpicId(id);
+        save();
     }
 
     @Override
     public void removeTasks() {
         super.removeTasks();
+        save();
     }
 
     @Override
     public void removeEpics() {
         super.removeEpics();
+        save();
     }
 
     @Override
     public void removeSubtasks() {
         super.removeSubtasks();
-    }
-
-    @Override
-    public Map<Long, Task> getTasks() {
-        return super.getTasks();
-    }
-
-    @Override
-    public Map<Long, Epic> getEpics() {
-        return super.getEpics();
-    }
-
-    @Override
-    public Map<Long, Subtask> getSubtasks() {
-        return super.getSubtasks();
-    }
-
-    @Override
-    public List<Task> getHistory() {
-        return super.getHistory();
+        save();
     }
 
     public void save() {
@@ -156,25 +165,27 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
             for (Subtask subtask : getSubtasks().values()) {
                 fw.write(toString(subtask) + "\n");
             }
-            fw.write("***\n"); // строка разделитель
-            fw.write(historyToString(super.historyManager));
+            if (!getHistory().isEmpty()) {
+                fw.write("***\n"); // строка разделитель
+                fw.write(historyToString(super.historyManager));
+            }
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            throw new ManagerSaveException("Ошибка чтения/записи файла");
         }
     }
 
-    public static FileBackedTasksManager loadFromFile() {
+    public static FileBackedTasksManager loadFromFile(String filePath) {
         FileBackedTasksManager fm = new FileBackedTasksManager();
-        try (BufferedReader br = new BufferedReader(new FileReader(PATH_FILE))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             br.readLine(); // Пропуск 1 строки с названиями столбцов
             while (br.ready()) {
                 String line = br.readLine();
                 if (line.equals("***")) {
                     line = br.readLine();
-                    for (Integer historyId : historyFromString(line)){
+                    for (Integer historyId : historyFromString(line)) {
                         fm.historyManager.add(fm.getTask(historyId));
                     }
-                } else{
+                } else {
                     Task entity = fromString(line);
                     if (entity instanceof Subtask) {
                         fm.newSubtask((Subtask) entity);
@@ -187,7 +198,7 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
             }
 
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            throw new ManagerSaveException("Ошибка чтения/записи файла");
         }
         return fm;
     }
